af6a943316855db45bd839cbc9bf55c2531f9b80
CaChiJ <53603478+CaChiJ@users.noreply.github.com>
2021-08-02 20:03:17 +0900

    script.js 파일 css 일부 분리 + 현재 페이지 표시

--- script.js
+++ script.js
@@ -3,33 +3,39 @@
 // ["이름", "스터디 목표", "프로젝트 개선 사항", "수행 여부", "비고"]
 // "" 안에 HTML 태그 써도 적용됨.
 
+const FAIL_SIGN = "❌";
+const UNDEFINED_SIGN = "💬";
+const SUCCESS_SIGN = "✔";
+const PAGE_COUNT = 5;
+let current = 3;
+
 const dh_contents = [
-    ["김동혁", "파이썬 정규표현식 학습<br/>구름EDU 파이썬 강좌 하나 수료", "[TEXTI] 이중 복수표현 삭제 후 뒤에 나오는 조사 자동으로 변경", "❌", ""], // 7월 셋째주
-    ["김동혁", "구름EDU 파이썬 강좌 두개 수료<br/>윗몸일으키기, 팔굽혀펴기 하루 50개", "워드프로세서 자격증 공부", "💬", ""], // 7월 넷째주
-    ["김동혁", "", "", "❌", ""], // 8월 첫째주
-    ["김동혁", "", "", "❌", ""], // 8월 둘째주
-    ["김동혁", "", "", "❌", ""]]; // 8월 셋째주
+    ["김동혁", "파이썬 정규표현식 학습<br/>구름EDU 파이썬 강좌 하나 수료", "[TEXTI] 이중 복수표현 삭제 후 뒤에 나오는 조사 자동으로 변경", FAIL_SIGN, ""], // 7월 셋째주
+    ["김동혁", "구름EDU 파이썬 강좌 두개 수료<br/>윗몸일으키기, 팔굽혀펴기 하루 50개", "워드프로세서 자격증 공부", UNDEFINED_SIGN, ""], // 7월 넷째주
+    ["김동혁", "", "", FAIL_SIGN, ""], // 8월 첫째주
+    ["김동혁", "", "", FAIL_SIGN, ""], // 8월 둘째주
+    ["김동혁", "", "", FAIL_SIGN, ""]]; // 8월 셋째주
 
 const dy_contents = [
-    ["김동연", "JS 학습 (script 태그 ~ 문자열)", "?", "💬", ""],
-    ["김동연", "", "", "💬", ""],
-    ["김동연", "", "", "❌", ""],
-    ["김동연", "", "", "❌", ""],
-    ["김동연", "", "", "❌", ""]];
+    ["김동연", "JS 학습 (script 태그 ~ 문자열)", "?", UNDEFINED_SIGN, ""],
+    ["김동연", "", "", UNDEFINED_SIGN, ""],
+    ["김동연", "", "", FAIL_SIGN, ""],
+    ["김동연", "", "", FAIL_SIGN, ""],
+    ["김동연", "", "", FAIL_SIGN, ""]];
 
 const mg_contents = [
-    ["전민규", "'밑바닥부터 시작하는 딥러닝 1' 훑기<br/>딥러닝 강의 1일 1강씩 수강", "[TEXTI] 서버 내부의 정적 파일(CSS 등) 참조 가능하도록 수정", "❌", ""],
-    ["전민규", "C++ 알고리즘 문제 1일 2문제 풀기<br/>딥러닝 강의 1일 1강씩 수강", "미정", "💬", ""],
-    ["전민규", "", "", "❌", ""],
-    ["전민규", "", "", "❌", ""],
-    ["전민규", "", "", "❌", ""]];
+    ["전민규", "'밑바닥부터 시작하는 딥러닝 1' 훑기<br/>딥러닝 강의 1일 1강씩 수강", "[TEXTI] 서버 내부의 정적 파일(CSS 등) 참조 가능하도록 수정", FAIL_SIGN, ""],
+    ["전민규", "C++ 알고리즘 문제 1일 2문제 풀기<br/>딥러닝 강의 1일 1강씩 수강", "미정", UNDEFINED_SIGN, ""],
+    ["전민규", "", "", FAIL_SIGN, ""],
+    ["전민규", "", "", FAIL_SIGN, ""],
+    ["전민규", "", "", FAIL_SIGN, ""]];
 
 const hj_contents = [
-    ["최현준", "'C Programming' 1일 1챕터 학습<br/>POCU 공학용 수학 몰아 듣기<br/>노마드 코더 자바스크립트 기초 파트 수강<br/>Andrew Ng AI 강의 4주치 몰아 듣기", "[TEXTI] HTML 코드에서 난잡한 부분 CSS, JS 파일로 분리", "❌", ""],
-    ["최현준", "C++ 알고리즘 문제 1일 2문제 풀기<br/>Andrew Ng AI 강의 4주치 수강<br/>'C Programming' 학습<br/>노마드 코더 SCSS 수강<br/>정보처리기능사 자격증 Part1 학습", "미정", "💬", ""],
-    ["최현준", "", "", "❌", ""],
-    ["최현준", "", "", "❌", ""],
-    ["최현준", "", "", "❌", ""]];
+    ["최현준", "'C Programming' 1일 1챕터 학습<br/>POCU 공학용 수학 몰아 듣기<br/>노마드 코더 자바스크립트 기초 파트 수강<br/>Andrew Ng AI 강의 4주치 몰아 듣기", "[TEXTI] HTML 코드에서 난잡한 부분 CSS, JS 파일로 분리", FAIL_SIGN, ""],
+    ["최현준", "C++ 알고리즘 문제 1일 2문제 풀기<br/>Andrew Ng AI 강의 4주치 수강<br/>'C Programming' 학습<br/>노마드 코더 SCSS 수강<br/>정보처리기능사 자격증 Part1 학습", "미정", UNDEFINED_SIGN, ""],
+    ["최현준", "", "", FAIL_SIGN, ""],
+    ["최현준", "", "", FAIL_SIGN, ""],
+    ["최현준", "", "", FAIL_SIGN, ""]];
 
 // ===== 여기까지 수정부분
 
@@ -66,10 +72,11 @@ function init(){
 }
 
 function setPage(){
-    if(index == 0) left.style.color = "gray";
-    else left.style.color = "black";
-    if(index == 4) right.style.color = "gray";
-    else right.style.color = "black";
+    if(index == 0) left.classList.add("last_page");
+    else left.classList.remove("last_page");
+
+    if(index == PAGE_COUNT-1) right.classList.add("last_page");
+    else right.classList.remove("last_page");
     
     inText = "<td class='txt'>{0}</td><td class='txt'>{1}</td><td class='txt'>{2}</td><td class='txt'>{3}</td><td class='txt'>{4}</td>";
         
@@ -79,18 +86,23 @@ function setPage(){
     hj.innerHTML = format(inText, hj_contents[index]);
     
     date.innerText = dates[index];
+
+    let current_page = document.getElementById("current_page");
+    current_page.innerText = `${current} / ${PAGE_COUNT}`;
 }
 
 left.addEventListener('click', function() {
     if(index > 0){
         index -= 1;
+        current--;
         setPage();
     }
 });
 
 right.addEventListener('click', function() {
-    if(index < 4){
+    if(index < PAGE_COUNT-1){
         index += 1;
+        current++;
         setPage();
     }
 });
